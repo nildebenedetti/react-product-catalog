f231a71923f34432a0170962c95b266f1d1e1212
“Nilde <nilde.benedetti@gmail.com>
2026-05-11 17:56:27 +0200

    spostata logica di useEffect dentro la componente productList

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,8 +1,33 @@
-import ProductCard from "./ProductCard"
+import ProductCard from "./ProductCard";
+import SearchBar from "./SearchBar";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function ProductList() {
-  return (
-    <div>ProductList</div>
-  )
+    {/** mi setto una variabile di stato per la lista di prodotti 
+    di modo che sia dinamica e aggiornata all'avvio di app ed 
+    eventuali trigger che verranno aggiunti in futuro*/}
+  const [ productList, setProductList] = useState([]);
+
+  useEffect (() => {
+    const URL_API = 'https://fakestoreapi.com/products';
+
+    fetch(URL_API)
+    .then ( response => {
+      return response.json();
+    })
+    .then ( json => {
+      // belli i dati, ora li assegno alla variabile di stato productList
+      setProductList(json);
+    })
+    .catch (error => {
+      console.error('Errore nel caricamento dei dati')
+      throw error;
+    })
+  },[]);
+
+
+  return <div>ProductList</div>
+  
 }
 export default ProductList
