88ecbd87b64f9057ee4fed37e071e77e04d4eefb
“Nilde <nilde.benedetti@gmail.com>
2026-05-11 17:56:55 +0200

    fix format in ProductList.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,8 @@ import { useState } from "react";
 
 function App() {
 
-  {/** mi setto una variabile di stato per la lista di prodotti 
-    di modo che sia dinamica e aggiornata all'avvio di app ed 
-    eventuali trigger che verranno aggiunti in futuro*/}
-  const [ productList, setProductList] = useState([]);
-
-  useEffect (() => {
-    const URL_API = 'https://fakestoreapi.com/products';
-
-    fetch(URL_API)
-    .then ( response => {
-      return response.json();
-    })
-    .then ( json => {
-      // belli i dati, ora li assegno alla variabile di stato productList
-      setProductList(json);
-    })
-    .catch (error => {
-      console.error('Errore nel caricamento dei dati')
-      throw error;
-    })
-  },[]);
-
-  return <div className="wrapper">
+    return <div className="wrapper">
     <ProductList/>
-     <div>
-      {JSON.stringify(productList)}
-      </div>
   </div>
 }
 export default App;
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -7,27 +7,27 @@ function ProductList() {
     {/** mi setto una variabile di stato per la lista di prodotti 
     di modo che sia dinamica e aggiornata all'avvio di app ed 
     eventuali trigger che verranno aggiunti in futuro*/}
-  const [ productList, setProductList] = useState([]);
+    const [productList, setProductList] = useState([]);
 
-  useEffect (() => {
-    const URL_API = 'https://fakestoreapi.com/products';
+    useEffect(() => {
+        const URL_API = 'https://fakestoreapi.com/products';
 
-    fetch(URL_API)
-    .then ( response => {
-      return response.json();
-    })
-    .then ( json => {
-      // belli i dati, ora li assegno alla variabile di stato productList
-      setProductList(json);
-    })
-    .catch (error => {
-      console.error('Errore nel caricamento dei dati')
-      throw error;
-    })
-  },[]);
+        fetch(URL_API)
+            .then(response => {
+                return response.json();
+            })
+            .then(json => {
+                // belli i dati, ora li assegno alla variabile di stato productList
+                setProductList(json);
+            })
+            .catch(error => {
+                console.error('Errore nel caricamento dei dati')
+                throw error;
+            })
+    }, []);
 
 
-  return <div>ProductList</div>
-  
+    return <div>ProductList</div>
+
 }
 export default ProductList
