8ddeda4f06cc953b43bfe820151f2fb4d739bfe8
“Nilde <nilde.benedetti@gmail.com>
2026-05-11 17:11:31 +0200

    add: import di use effect e use state hooks in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import ProductList from "./components/ProductList";
 import SearchBar from "./components/SearchBar";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
   return <div className="wrapper">
